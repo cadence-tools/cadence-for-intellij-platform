eafb8de6347594a26c2c39ed1db942c138730275
Nikitas Kotsolakos <nikitas.kotsolakos@cern.ch>
2021-10-15 08:14:04 +0200

    Improve demo settings text

--- src/main/java/cadence/language/syntax/highlighting/CadenceColorSettingsPage.java
+++ src/main/java/cadence/language/syntax/highlighting/CadenceColorSettingsPage.java
@@ -44,19 +44,17 @@ public class CadenceColorSettingsPage implements ColorSettingsPage {
 
     @NotNull
     @Override
-    public String getDemoText() { //TODO put a nice demo text
-        return "# You are reading the \".properties\" entry.\n" +
-            "! The exclamation mark can also mark text as comments.\n" +
-            "website = https://en.wikipedia.org/\n" +
-            "language = English\n" +
-            "# The backslash below tells the application to continue reading\n" +
-            "# the value onto the next line.\n" +
-            "message = Welcome to \\\n" +
-            "          Wikipedia!\n" +
-            "# Add spaces to the key\n" +
-            "key\\ with\\ spaces = This is the value that could be looked up with the key \"key with spaces\".\n" +
-            "# Unicode\n" +
-            "tab : \\u0009";
+    public String getDemoText() {
+        return "// Single line comments\n" +
+            "///Single line doc comments\n" +
+            "/* Block\n" +
+            " comments */\n" +
+            "/** Block Doc\n" +
+            " comments */\n" +
+            "let mytext: String = \"test string escape \\n or escape \\u{0009} unicode\"\n" +
+            "pub fun mytestfun(amount: Int32, addresses: @Address){\n" +
+            " amount = 525;\n" +
+            " return amount; }\n";
     }
 
     @Nullable
